d6d7c15dcd006d6d627fa76e74c7096043ecf2b7
Conor <c17730535@mydit.ie>
2022-12-04 23:07:06 +0000

    Add text file and iterator method

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿namespace TeleprompterConsole;
+
+internal class Program{
+  static void Main(string[] args){
+    var lines = ReadFrom("sampleQuotes.txt");
+    foreach (var line in lines){
+      System.Console.WriteLine(line);
+    }
+  }
+
+  static IEnumerable<string> ReadFrom(string file){
+    string? line;
+    using(var reader = File.OpenText(file)){
+      while((line = reader.ReadLine()) != null){
+        yield return line;
+      }
+    }
+  }
+}
