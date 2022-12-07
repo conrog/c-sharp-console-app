8d1aff771b238a2ab30e236f24de9fbdc1677a09
Conor <c17730535@mydit.ie>
2022-12-07 20:34:07 +0000

    finished implementation

--- Program.cs
+++ Program.cs
@@ -1,19 +1,64 @@
 ﻿namespace TeleprompterConsole;
 
 internal class Program{
-  static void Main(string[] args){
-    var lines = ReadFrom("sampleQuotes.txt");
-    foreach (var line in lines){
-      System.Console.WriteLine(line);
-    }
+  static async Task Main(string[] args){
+    await RunTeleprompter();
   }
 
   static IEnumerable<string> ReadFrom(string file){
     string? line;
     using(var reader = File.OpenText(file)){
       while((line = reader.ReadLine()) != null){
-        yield return line;
+        var words = line.Split(' ');
+        var lineLength = 0;
+
+        foreach(var word in words){
+          yield return word + " ";
+          lineLength += word.Length + 1;
+          if(lineLength > 70){
+            yield return Environment.NewLine;
+            lineLength = 0;
+          }
+        }
+        yield return Environment.NewLine;
       }
     }
+  }
+
+  private static async Task ShowTeleprompter(TelePrompterConfig config){
+    var words = ReadFrom("sampleQuotes.txt");
+
+    foreach (var word in words){
+      System.Console.Write(word);
+      if(!string.IsNullOrWhiteSpace(word)){
+        await Task.Delay(config.DelayInMilliseconds);
+      }
+    }
+    config.SetDone();
+  }
+
+  private static async Task GetInput(TelePrompterConfig config){    
+    Action work = () => {
+      do{
+        var key = Console.ReadKey(true);
+        if (key.KeyChar == '>'){
+          config.UpdateDelay(-10);
+        } else if (key.KeyChar == '<'){
+          config.UpdateDelay(10);
+        } else if (key.KeyChar == 'X' || key.KeyChar == 'x'){
+          config.SetDone();
+        }
+      }while(!config.Done);
+    };
+
+    await Task.Run(work);
+  }
+
+  private static async Task RunTeleprompter(){
+    var config = new TelePrompterConfig();
+    var displayTask = ShowTeleprompter(config);
+
+    var speedTask = GetInput(config);
+    await Task.WhenAny(displayTask, speedTask);
   }
 }
